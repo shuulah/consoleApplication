e039cdfa9313bef348ee9ba7fcd843075fca4719
shuulah <xisali1998@gmail.com>
2025-03-07 01:57:55 +0100

    modified

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,6 +3,7 @@ package org.example;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator is to be developed");
+        System.out.println("Do not forget to share to github");
 
     }
 }
