5eadbe319cb7aec385d7518b83349d563c01c887
shuulah <xisali1998@gmail.com>
2025-03-07 03:04:17 +0100

    added methods

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,9 +1,18 @@
 package org.example;
 
 public class Calculator {
+    public static int add(int num1, int num2){
+        return num1 +num2;
+    }
+    public static double multiply(double num1, double num2){
+        return num1*num2;
+    }
+    public static double div(double num1, double num2){
+        return (num1/num2);
+    }
     public static void main(String[] args) {
-        System.out.println("Calculator is to be developed");
-        System.out.println("Do not forget to share to github");
+
+
 
     }
 }
