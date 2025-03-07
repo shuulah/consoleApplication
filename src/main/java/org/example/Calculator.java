df8e1dba6b32961b53d6bd40230b642a5e07ea52
shuulah <xisali1998@gmail.com>
2025-03-07 03:08:58 +0100

    added new methods

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     public static int add(int num1, int num2){
         return num1 +num2;
     }
+    //
     public static double multiply(double num1, double num2){
         return num1*num2;
     }
