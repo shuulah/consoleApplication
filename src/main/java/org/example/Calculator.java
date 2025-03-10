a961678a66e7d8f053d1786ee8dfd4ffb3ab0dfe
shuulah <xisali1998@gmail.com>
2025-03-10 09:07:48 +0100

    Done

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,29 +2,32 @@ package org.example;
 
 import java.util.Scanner;
 
-import static java.lang.System.exit;
-
 public class Calculator {
     public static int add(int num1, int num2){
-        return num1 +num2;
+        return num1 + num2;
     }
     public static int sub(int num1, int num2){
         return num1 - num2;
     }
     public static double multiply(double num1, double num2){
-        return num1*num2;
+        return num1 * num2;
     }
     public static double div(double num1, double num2){
+        if(num2==0){
+            throw new ArithmeticException("division by zero not allowed");
+        }
         return (num1/num2);
     }
     public static void main(String[] args) {
-        Scanner operator= new Scanner(System.in);
-        //System.out.println("input your desired operation to perform :addition, multiplication and division");
-        //char operator= userInput.next().charAt(0);
+        Scanner scanner= new Scanner(System.in);
         while (true) {
-            System.out.println("input your desired operation to perform :addition, multiplication and division");
-            char option= operator.next().charAt(0);
-            if (option=='+'){
+            System.out.println("input your desired operation to perform :+, * , - and / OR q to exit");
+            char operator= scanner.next().charAt(0);
+            if (operator=='q'){
+                System.out.println("Exit the program");
+                break;
+            }
+           /* if (option=='+'){
                 System.out.println("Please input two numbers:");
                 Scanner userInput= new Scanner(System.in);
                 int input= userInput.nextInt();
@@ -62,15 +65,32 @@ public class Calculator {
             else {
                 System.out.println("operator is not allowed");
                 System.out.println("please choose 0 as an operator to end the program");
-            }
-            if (option=='0'){
-                exit(0);
-            }
+            }*/
+            if (operator!='+' && operator!='-' && operator!='*' && operator!='/'){
+                System.out.println("not allowed operator");
+                continue;
 
+            }
+            System.out.println("Please input two numbers:");
+            double input= scanner.nextInt();
+            double input2= scanner.nextInt();
+             switch (operator) {
+                 case '+':
+                     System.out.println("result: " + add((int) input, (int) input2));
+                     break;
+                 case '-':
+                     System.out.println("result: " + sub((int) input, (int) input2));
+                     break;
+                 case '*':
+                     System.out.println("result: " + multiply(input, input2));
+                     break;
+                 case '/':
+                     System.out.println("result: " + div(input, input2));
+                     break;
+             }
 
         }
-
-
+        scanner.close();
 
 
     }
